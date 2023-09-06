b0863161e453bbad8bb216a466f70526d2974afb
Ramesh <101050352+RAmxsda@users.noreply.github.com>
2023-09-06 21:56:20 +0545

    I don't know what the hell problem is happening with wait_for_db

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""Calculator functions"""
+
+
+def add(x, y):
+    """Addd x and y and returns the result"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and return result"""
+    return y - x
